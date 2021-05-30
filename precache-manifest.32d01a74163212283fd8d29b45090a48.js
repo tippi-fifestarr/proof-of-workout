bd4ad93b785b2ddc96641d3460668066d26b43a1
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-05-29 19:38:06 -0500

    Updates

---
+++ precache-manifest.32d01a74163212283fd8d29b45090a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606f84192fa938fd67e3e26890434bb6",
+    "url": "/proof-of-workout/index.html"
+  },
+  {
+    "revision": "2fda3380815d7a2b64a2",
+    "url": "/proof-of-workout/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "d481fc2a23b73919c117",
+    "url": "/proof-of-workout/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "2fda3380815d7a2b64a2",
+    "url": "/proof-of-workout/static/js/2.e2b93dc6.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/proof-of-workout/static/js/2.e2b93dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d481fc2a23b73919c117",
+    "url": "/proof-of-workout/static/js/main.37bae0ea.chunk.js"
+  },
+  {
+    "revision": "bfc41402d1d42c22f859",
+    "url": "/proof-of-workout/static/js/runtime-main.69737984.js"
+  }
+]);
